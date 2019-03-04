d74543591a5fa8f06bf761669c30435b1ffc9663
David Ackerman <enfipy@gmail.com>
2019-03-04 22:39:09 +0200

    Add hash func

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/enfipy/byhash
+
+require golang.org/x/crypto v0.0.0-20190228161510-8dd112bcdc25
--- src/main.go
+++ src/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"log"
+
+	"golang.org/x/crypto/sha3"
+)
+
 func main() {
-	println("By hash?")
+	log.Println("Running byhash utility")
+
+	data := "hello world"
+	hash := toKeccak([]byte(data))
+
+	log.Printf("%x\n", hash)
+}
+
+func toKeccak(data []byte) []byte {
+	hash := make([]byte, 64)
+
+	keccak := sha3.NewLegacyKeccak256()
+	keccak.Write(data)
+	hash = keccak.Sum(nil)
+
+	return hash
 }
